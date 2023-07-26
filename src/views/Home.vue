b1adc71be8debb770d8cb3919931502667631f29
Ameerali C <ameeralic95@gmail.com>
2023-07-26 19:24:21 +0530

    some edits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
     <PageLayout>
         <Hero :projectName="projectName" :projectLogo="projectLogo" :projectDescription="projectDescription"
-            :projectStack="projectStack" :whatsNewText="'New LMS Project Available in LaraVueTail'"
-            :whatsNewLink="'https://laravuetail.com/'"
-            :projectGitRepoLink="'https://github.com/LaraVueTail/Restaurant-Review-Generator-OpenAI-LaravelAPI-Vue-Tailwind'">
+            :projectStack="projectStack" :whatsNewText="projectWhatsNewText" :whatsNewLink="'https://laravuetail.com/'"
+            :projectGitRepoLink="projectGitRepoLink">
         </Hero>
         <WatchVideo :projectDemoLink="projectDemoLink" :projectDemoVideo="projectDemoVideo"></WatchVideo>
         <WhatIsThis></WhatIsThis>
@@ -26,6 +25,7 @@ export default {
             projectStack: import.meta.env.VITE_PROJECT_STACK,
             projectDemoLink: import.meta.env.VITE_PROJECT_DEMO_LINK,
             projectDemoVideo: import.meta.env.VITE_PROJECT_DEMO_VIDEO,
+            projectWhatsNewText: import.meta.env.VITE_PROJECT_WHATS_NEW_TEXT,
         }
     },
 }
